c4c1307bb381eb93f3f2c6b77a3a4e5fb34d4756
kimsangmin <ksm619@naver.com>
2021-01-29 01:22:58 +0900

    Programmers

--- Level_2/Open_chat.py
+++ Level_2/Open_chat.py
@@ -6,18 +6,30 @@ online = []
 result = ""
 for i in range(len(record)):
     if "Enter" in record[i]:
-        result += "%s님이 들어왔습니다. %s " % (record[i][2], record[i][1])
+        result += "%s님이 들어왔습니다. %s \n" % (record[i][2], record[i][1])
         online.append([record[i][1], record[i][2]])
-        print("online", online)
     elif "Leave" in record[i]:
         for j in range(len(online)):
             if record[i][1] in online[j]:
-                result += "%s님이 나갔습니다. %s " % (online[j][1], online[j][0])
-        # online = online.replace()
+                result += "%s님이 나갔습니다. %s \n" % (online[j][1], online[j][0])
+                break
+        del online[j]
+
     elif "Change" in record[i]:
-        result += "%s님이 변경하였습니다. %s " % (record[i][2], record[i][1])
-        for k in range(i):
-            if record[i][1] in
+        result += "%s님이 변경하였습니다. %s \n" % (record[i][2], record[i][1])
+        for j in range(len(result)):
+        
+            if record[i][1] in result[j]:
+                result[j] = result[j].replace(record[j][2], record[i][2])
 
-
+    # print('online', online)
+    # print(result)
 print(result)
+
+
+
+
+# Muzi님이 들어왔습니다.     Muzi님이 들어왔습니다.     Prodo님이 들어왔습니다.         Prodo님이 들어왔습니다.
+# Prodo님이 들어왔습니다     Prodo님이 들어왔습니다.    Prodo님이 들어왔습니다.         Ryan님이 들어왔습니다.
+#                           Muzi님이 나갔습니다.       Prodo님이 나갔습니다.           Prodo님이 나갔습니다.
+#                                                     Prodo님이 들어왔습니다.         Prodo님이 들어왔습니다
---
+++ Level_2/test.py
@@ -0,0 +1,41 @@
+# # 가장 큰 수
+#
+# from itertools import permutations
+#
+# numbers = [3, 30, 34, 5, 9]
+# length = [x for x in range(len(numbers))]
+# print(length)
+# max = 0
+# for i in permutations(length, len(numbers)):
+#     n = ""
+#     for j in range(len(numbers)):
+#         n += str(numbers[i[j]])
+#     if max < int(n):
+#         max = int(n)
+
+
+# 짝지어 제거하기
+
+def solution(s):
+    answer = 0
+    i = 0
+    while len(s) > i+1:
+        if s[i] == s[i+1]:
+            s = s.replace(s[i:i+2], "")
+            i = i-1 if i >= 1 else 0
+        else :
+            i += 1
+
+    if len(s) == 0:
+        answer = 1
+
+
+
+def solution(s):
+    stack = []
+    for i in s:
+        if len(stack) == 0: stack.append(i)
+        elif stack[-1] == i: stack.pop()
+        else: stack.append(i)
+    if len(stack) == 0: return 1
+    else: return 0
